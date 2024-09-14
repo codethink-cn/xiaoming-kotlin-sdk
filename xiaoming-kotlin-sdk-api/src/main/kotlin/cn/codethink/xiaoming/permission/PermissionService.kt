b4d2fd657f842877e587cabe45398591edb2573d
chuanwise <chuanwise@qq.com>
2024-09-14 14:09:39 +0800

    feat(api): add `PermissionService`.

---
+++ xiaoming-kotlin-sdk-api/src/main/kotlin/cn/codethink/xiaoming/permission/PermissionService.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.permission
+
+import cn.codethink.xiaoming.common.Matcher
+import cn.codethink.xiaoming.common.Subject
+
+/**
+ * Operations related to [Permission] and [PermissionMeta].
+ *
+ * @author Chuanwise
+ */
+interface PermissionService {
+    /**
+     * Test if the subject has the permission.
+     *
+     * @return `null` if the permission is not found.
+     */
+    fun hasPermission(subject: Subject, permissionMatcher: Matcher<Permission>): Boolean?
+}
+
+fun PermissionService.hasPermission(subject: Subject, permission: Permission): Boolean? {
+    return hasPermission(subject, permission.toLiteralMatcher())
+}
