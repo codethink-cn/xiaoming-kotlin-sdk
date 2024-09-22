12906b46d4991001f9febbc97d83d7437d0364cc
chuanwise <chuanwise@qq.com>
2024-09-22 11:32:07 +0800

    feat(api): add `Throwable.rootCause` util.

---
+++ xiaoming-kotlin-sdk-api/src/main/kotlin/cn/codethink/xiaoming/common/Exceptions.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024 CodeThink Technologies and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.codethink.xiaoming.common
+
+/**
+ * Find the deepest cause of current exception.
+ */
+val Throwable.rootCause: Throwable
+    get() {
+        var result = this
+        while (result.cause != null) {
+            result = result.cause!!
+        }
+        return result
+    }
