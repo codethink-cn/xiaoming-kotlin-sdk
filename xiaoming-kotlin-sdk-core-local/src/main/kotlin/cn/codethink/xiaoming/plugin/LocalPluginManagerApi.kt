16cd137c967180f0f7e93a591037cf2d743e5cdd
chuanwise <chuanwise@qq.com>
2024-10-04 23:24:57 +0800

    feat(core-local): add uncompleted `enablePlugins`.

--- xiaoming-kotlin-sdk-core-local/src/main/kotlin/cn/codethink/xiaoming/plugin/LocalPluginManagerApi.kt
+++ xiaoming-kotlin-sdk-core-local/src/main/kotlin/cn/codethink/xiaoming/plugin/LocalPluginManagerApi.kt
@@ -17,6 +17,7 @@
 package cn.codethink.xiaoming.plugin
 
 import cn.codethink.xiaoming.common.Cause
+import cn.codethink.xiaoming.common.Id
 import cn.codethink.xiaoming.common.InternalApi
 import cn.codethink.xiaoming.common.SubjectDescriptor
 import cn.codethink.xiaoming.common.providedOrFromCurrentThread
@@ -29,19 +30,65 @@ class LocalPluginManagerApi(
 ) {
     val lock = ReentrantReadWriteLock()
 
+    private val mutablePlugins: MutableMap<Id, Plugin> = HashMap()
+    val plugins: Map<Id, Plugin>
+        get() = mutablePlugins.toMap()
+
     @InternalApi
     internal fun start(cause: Cause, subject: SubjectDescriptor) {
 
     }
 
-    fun enablePlugin(plugin: Plugin, cause: Cause, subject: SubjectDescriptor) = enablePlugins(
-        listOf(plugin), cause, subject
-    )
-
-    fun enablePlugins(plugins: Iterable<Plugin>, cause: Cause, subject: SubjectDescriptor): Unit = lock.write {
+    fun enablePlugins(
+        plugins: Iterable<Plugin>,
+        force: Boolean = false,
+        cause: Cause? = null,
+        subject: SubjectDescriptor? = null
+    ): Unit = lock.write {
         val (causeOrDefault, subjectOrDefault) = providedOrFromCurrentThread(cause, subject)
-        val pluginsById = plugins.associateBy { it.subject.id }
+        val pluginsById = plugins.associateBy { it.descriptor.id }
 
-        TODO()
+        // Build dependency graph.
+        class Node(
+            val plugin: Plugin,
+            val present: Boolean,
+
+            // Other -> This
+            val edgeHeads: MutableMap<Id, Node> = mutableMapOf(),
+
+            // This -> Other
+            val edgeTails: MutableMap<Id, Node> = mutableMapOf()
+        )
+
+        val nodes = mutableMapOf<Id, Node>()
+
+        // 1. Check if there is any plugin that is in error state.
+        for (plugin in plugins) {
+            val presented = mutablePlugins[plugin.descriptor.id]
+            if (presented != null) {
+                // Check if version changed.
+                if (presented.version != plugin.version) {
+                    // If version changed, we need to unload the previous one.
+                    // Then load and enable the new one.
+
+                    if (presented.isErrored) {
+                        check(!force) {
+                            "The plugins to enabled contains '${plugin.toExactRequirement()}', " +
+                                    "but there is a same plugin with a another version '${presented.version}', " +
+                                    "platform needs to unload it first. " +
+                                    "But it is already in an error state and the platform dare not forcefully operate it. " +
+                                    "If you really need to enable the new, please set the `force` to true " +
+                                    "or disable the presented forcefully first."
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // 1. Add present plugins.
+        for (presentPlugin in this.mutablePlugins) {
+            nodes[presentPlugin.key] = Node(presentPlugin.value, true)
+        }
     }
 }
