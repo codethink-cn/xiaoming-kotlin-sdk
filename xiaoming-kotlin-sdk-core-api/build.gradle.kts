7a554e082555dace0b0ad7e15a95f52fd8f17fda
chuanwise <chuanwise@qq.com>
2025-05-13 10:13:09 +0800

    fix(api): remove extra copyright head, fix build config name.

--- xiaoming-kotlin-sdk-core-api/build.gradle.kts
+++ xiaoming-kotlin-sdk-core-api/build.gradle.kts
@@ -16,22 +16,6 @@
 
 import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 
-/*
- * Copyright 2025 CodeThink Technologies and contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.blocking.bridge)
@@ -47,7 +31,7 @@ buildConfig {
     packageName("cn.codethink.xiaoming")
 
     string("GROUP", group)
-    string("NAME", name)
+    string("NAME", "xiaoming-kotlin-sdk-core-api")
     string("VERSION", version)
 
     string("STANDARD_VERSION", "0.1.0")
